ac294c28fe5fc5b88f013f2e00b121f60841df48
dganai <dinaganai@gmail.com>
2022-02-20 18:18:15 -0500

    add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import PlayerList from './components/PlayerList';
+import MatchList from './components/MatchList';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>
+				Tourney Matches{' '}
+				<span>Where Coding and Tournaments found their Match!</span>
+			</h1>
+			<PlayerList />
+			<MatchList />
+		</div>
+	);
 }
 
 export default App;
