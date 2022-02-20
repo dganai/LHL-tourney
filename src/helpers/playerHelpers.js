29ba893110d752cab62afa570c3000caa09db1a1
dganai <dinaganai@gmail.com>
2022-02-20 18:44:53 -0500

    implement helper functions

--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -1,3 +1,21 @@
-export const preparePlayerData = () => {};
+export const preparePlayerData = (playerData) => {
+	return Object.values(playerData);
+};
 
-export const addWinsToPlayers = () => {};
+export const addWinsToPlayers = (playerDataArray, matchData) => {
+	return playerDataArray.map((player) => {
+		// calculate number of wins in matchData
+		const currentWins = matchData.reduce((accumulator, match) => {
+			// adds a win if gamertag matches
+			if (match.winner === player.gamerTag) {
+				return (accumulator += 1);
+			} else {
+				return accumulator;
+			}
+		}, 0);
+
+		// assign value to wins key
+		player.wins = currentWins;
+		return player;
+	});
+};
