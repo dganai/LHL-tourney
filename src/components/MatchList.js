a859747dcfc266217f625d5525642a4a4efe3072
dganai <dinaganai@gmail.com>
2022-02-20 18:14:56 -0500

    import update to components

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react';
+import Match from './Match';
 
 function MatchList() {
-  return (
-     <section className="PlayerList MatchList">
-      <h1>Match list</h1>
-      {/* Matches will be shown here */}
-  </section>
-  )
+	return (
+		<section className="PlayerList MatchList">
+			<h1>Match list</h1>
+			<Match />
+		</section>
+	);
 }
 
 export default MatchList;
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
+import Player from './Player';
 
 function PlayerList(prop) {
-  return (
-    <section className="PlayerList">
-      <h1>Current participating players</h1>
-      {/* Players will be shown here */}
-    </section>
-  );
+	return (
+		<section className="PlayerList">
+			<h1>Current participating players</h1>
+			<Player />
+		</section>
+	);
 }
 export default PlayerList;
