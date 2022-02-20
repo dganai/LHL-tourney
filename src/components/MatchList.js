adc475fdffda1444c0af6ac7e3b57fa856af04ca
dganai <dinaganai@gmail.com>
2022-02-20 17:55:59 -0500

    components for app

---
+++ src/components/MatchList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MatchList() {
+  return (
+     <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      {/* Matches will be shown here */}
+  </section>
+  )
+}
+
+export default MatchList;
---
+++ src/components/PlayerList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PlayerList(prop) {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      {/* Players will be shown here */}
+    </section>
+  );
+}
+export default PlayerList;
