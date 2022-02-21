22962ee4a25748d1b7c7b263ee9239f2c8463a4a
dganai <dinaganai@gmail.com>
2022-02-20 19:34:30 -0500

    refactor to reflect app.js changes

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Match from './Match';
 
 function MatchList(props) {
-	const oneMatch = matchData[0];
+	const parsedMatches = Object.values(props).map(match => <Match {...match} />)
 	return (
 		<section className="PlayerList MatchList">
 			<h1>Match list</h1>
-			<Match {...oneMatch} />
+			{parsedMatches}
 		</section>
 	);
 }
