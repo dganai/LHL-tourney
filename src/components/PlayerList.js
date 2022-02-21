05b99ee2b1c876855680c27224b32c604c922750
dganai <dinaganai@gmail.com>
2022-02-20 19:44:46 -0500

    refactor

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Player from './Player';
 
 function PlayerList(props) {
-	const parsedPlayerList = Object.values(props).map((player) => (
-		<Player {...player} />
-	));
+	  const parsedPlayerList = parsedPlayerData.map((player) => (
+			<Player key={player.gamerTag} {...player} />
+		));
+
 	return (
 		<section className="PlayerList">
 			<h1>Current participating players</h1>
