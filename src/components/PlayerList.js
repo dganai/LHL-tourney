7158f08087789bc802bf62ce417b70bf381b6b04
dganai <dinaganai@gmail.com>
2022-02-20 19:21:45 -0500

    replace hardcoded values with data from props

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -4,13 +4,14 @@ import matchData from '../data/matchData';
 import playerData from '../data/playerData';
 import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
-function PlayerList(prop) {
+function PlayerList(props) {
 	const playerDataArray = preparePlayerData(playerData);
 	const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+	const onePlayer = parsedPlayerData[0];
 	return (
 		<section className="PlayerList">
 			<h1>Current participating players</h1>
-			<Player />
+			<Player {...onePlayer} />
 		</section>
 	);
 }
