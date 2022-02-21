cdb206d9de29abaa648a161a25d1fda5b66d57b2
dganai <dinaganai@gmail.com>
2022-02-20 19:36:19 -0500

    refactor to reflect app.js changes

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Player from './Player';
-import playerData from '../data/playerData';
-import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 function PlayerList(props) {
-	const playerDataArray = preparePlayerData(playerData);
-	const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
-	const onePlayer = parsedPlayerData[0];
+	const parsedPlayerList = Object.values(props).map((player) => (
+		<Player {...player} />
+	));
 	return (
 		<section className="PlayerList">
 			<h1>Current participating players</h1>
-			<Player {...onePlayer} />
+			{parsedPlayerList}{' '}
 		</section>
 	);
 }
