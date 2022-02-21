deb2e733ebdf54ba36e76d451b2385d9e3207ebf
dganai <dinaganai@gmail.com>
2022-02-20 19:12:50 -0500

    replace hardcoded values with data from props to show winner's name

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-function Match(prop) {
+function Match(props) {
+	const { players, winner, scoreDifference } = props;
+
 	return (
 		<article className="Match">
 			<h1>
-				Joe <span>vs</span> Bob
+				{players[0]}
+				<span>vs</span>
+				{players[1]}
 			</h1>
-			{/* To be shown when there is a winner */}
-			<h2>Joe is the winner by 1!</h2>
-			{/* To be shown when there is no winner */}
-			<h2>No winners yet!</h2>
+			{winner && (
+				<h2>
+					{winner} is the winner by {scoreDifference}
+				</h2>
+			)}
+			{!winner && <h2>No winners yet!</h2>}
 		</article>
 	);
 }
